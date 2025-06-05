7a8f76d426536144c71fab334dbbd424974b45e3
이호성 <hslee@tnear.com>
2025-06-05 11:40:54 +0900

    test: Persistence 통합 테스트

--- src/test/java/com/example/demo/member/adapter/out/persistence/MemberPersistenceAdapterTest.java
+++ src/test/java/com/example/demo/member/adapter/out/persistence/MemberPersistenceAdapterTest.java
@@ -1,29 +1,40 @@
 package com.example.demo.member.adapter.out.persistence;
 
 
+import com.example.demo.common.exception.BusinessException;
+import com.example.demo.config.JpaAuditingConfiguration;
 import com.example.demo.config.test.TestContainerConfig;
 import com.example.demo.member.domain.GenderEnum;
 import com.example.demo.member.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
-@Import({MemberPersistenceAdapter.class, MemberPersistenceMapperImpl.class})
+@Import({MemberPersistenceAdapter.class, MemberPersistenceMapperImpl.class, JpaAuditingConfiguration.class})
 @DisplayName("Member JPA Persistence Integration Test")
 public class MemberPersistenceAdapterTest extends TestContainerConfig {
 
     @Autowired
     private MemberPersistenceAdapter memberPersistenceAdapter;
 
-    @Test
-    void 회원_저장() {
+    private Member member;
+    private Member saveMember;
+
+
+    @BeforeEach
+    void setup() {
         // given
-        Member member = Member.builder()
+        member = Member.builder()
                 .email("user@example.com")
                 .password("QWERasdf1234!")
                 .name("홍길동")
@@ -31,20 +42,70 @@ public class MemberPersistenceAdapterTest extends TestContainerConfig {
                 .phoneNumber("010-1234-5678")
                 .address("서울특별시 강남구 테헤란로 123")
                 .build();
+        // when
+        saveMember = memberPersistenceAdapter.save(member);
+    }
+
+    @Test
+    void 회원_저장() {
+        // then
+        assertAll(
+                () -> assertThat(saveMember.getId()).isNotNull(),
+                () -> assertThat(member.getEmail()).isEqualTo(saveMember.getEmail()),
+                () -> assertThat(member.getPassword()).isEqualTo(saveMember.getPassword()),
+                () -> assertThat(member.getName()).isEqualTo(saveMember.getName()),
+                () -> assertThat(member.getGender()).isEqualTo(saveMember.getGender()),
+                () -> assertThat(member.getPhoneNumber()).isEqualTo(saveMember.getPhoneNumber()),
+                () -> assertThat(member.getAddress()).isEqualTo(saveMember.getAddress())
+        );
+    }
+
+    @Test
+    void 회원_조회() {
 
         // when
-        Member result = memberPersistenceAdapter.save(member);
+        Member resultMember = memberPersistenceAdapter.findById(saveMember.getId());
 
         // then
         assertAll(
-                () -> assertNotNull(result.getId()),
-                () -> assertEquals(member.getEmail(), result.getEmail()),
-                () -> assertEquals(member.getPassword(), result.getPassword()),
-                () -> assertEquals(member.getName(), result.getName()),
-                () -> assertEquals(member.getGender(), result.getGender()),
-                () -> assertEquals(member.getPhoneNumber(), result.getPhoneNumber()),
-                () -> assertEquals(member.getAddress(), result.getAddress())
+                () -> assertThat(resultMember.getId()).isNotNull(),
+                () -> assertThat(member.getEmail()).isEqualTo(resultMember.getEmail()),
+                () -> assertThat(member.getPassword()).isEqualTo(resultMember.getPassword()),
+                () -> assertThat(member.getName()).isEqualTo(resultMember.getName()),
+                () -> assertThat(member.getGender()).isEqualTo(resultMember.getGender()),
+                () -> assertThat(member.getPhoneNumber()).isEqualTo(resultMember.getPhoneNumber()),
+                () -> assertThat(member.getAddress()).isEqualTo(resultMember.getAddress())
         );
+    }
 
+    @Test
+    void 회원_목록() {
+
+        // when
+        List<Member> resultMemberList = memberPersistenceAdapter.findAll();
+
+        // then
+        assertAll(
+                () -> assertThat(resultMemberList).isNotEmpty(),
+                () -> assertThat(resultMemberList.getFirst().getId()).isNotNull(),
+                () -> assertThat(member.getEmail()).isEqualTo(resultMemberList.getFirst().getEmail()),
+                () -> assertThat(member.getPassword()).isEqualTo(resultMemberList.getFirst().getPassword()),
+                () -> assertThat(member.getName()).isEqualTo(resultMemberList.getFirst().getName()),
+                () -> assertThat(member.getGender()).isEqualTo(resultMemberList.getFirst().getGender()),
+                () -> assertThat(member.getPhoneNumber()).isEqualTo(resultMemberList.getFirst().getPhoneNumber()),
+                () -> assertThat(member.getAddress()).isEqualTo(resultMemberList.getFirst().getAddress())
+        );
+    }
+
+    @Test
+    void 회원_삭제() {
+
+        // when
+        memberPersistenceAdapter.deleteById(saveMember.getId());
+
+        assertAll(
+                () -> assertThatThrownBy(() -> memberPersistenceAdapter.findById(saveMember.getId()))
+                        .isInstanceOf(BusinessException.class)
+        );
     }
 }
