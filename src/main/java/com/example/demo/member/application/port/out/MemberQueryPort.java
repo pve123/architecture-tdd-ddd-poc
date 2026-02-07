5530484c36327d017d4bfccdf79facf0e4a0b6fb
pve123 <tkddlfdlfemd@naver.com>
2026-02-07 19:58:34 +0900

    feat: 사용자 페이지네이션 / 동적검색 / 정렬

--- src/main/java/com/example/demo/member/application/port/out/MemberQueryPort.java
+++ src/main/java/com/example/demo/member/application/port/out/MemberQueryPort.java
@@ -1,5 +1,6 @@
 package com.example.demo.member.application.port.out;
 
+import com.example.demo.member.adapter.in.web.request.MemberSearchRequest;
 import com.example.demo.member.domain.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,7 +8,8 @@ import org.springframework.data.domain.Pageable;
 public interface MemberQueryPort {
 
 
-    Page<Member> searchMembers(Pageable pageable);
+    Page<Member> searchMembers(Pageable pageable, MemberSearchRequest memberSearchRequest);
+
     Member findById(String id);
 
 }
